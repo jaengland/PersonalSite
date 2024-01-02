08193ba1d41795e8eba1d5d2f229b091826d1bcf
Jason England <jaengland1@gmail.com>
2024-01-01 22:34:56 -0600

    fix: add missing variables

--- infrastructure/site.tf
+++ infrastructure/site.tf
@@ -2,8 +2,16 @@
 module "site_s3_bucket" {
   source = "./modules/s3"
 
-  bucket_name       = ""
-  branch_name       = ""
-  kms_arn           = ""
+  bucket_name       = "jaengland-personal-site"
+  branch_name       = var.branch_name
+  kms_arn           = var.kms_arn
   bucket_versioning = "Disabled"
+}
+
+module "site_s3_bucket" {
+  source = "./modules/cloudfront"
+
+  branch_name = var.branch_name
+  bucket_regional_domain_name = module.site_s3_bucket.cloudfront_distribution_domain_name
+
 }
