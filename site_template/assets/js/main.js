a7c17dcfa310e266b5fb0f56bb00c6b724fab72d
Jason England <jaengland1@gmail.com>
2024-01-06 15:44:04 -0600

    feat: javascript unit tests with jest

--- site_template/assets/js/main.js
+++ site_template/assets/js/main.js
@@ -1,59 +1,19 @@
+import { selectFn, getRandomImage} from './utils.js';
+import { closeMobileNav, updateActiveTab, startTyped} from './nav.js';
+
 (function () {
   "use strict";
-
-  /**
-   * Element selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  function getRandomImage(substring) {
-    const images = [
-      'assets/img/photog1.jpg',
-      'assets/img/photog2.jpg',
-      'assets/img/photog3.jpg',
-    ];
-
-    let randomImage;
-    do {
-      // Select a random image
-      randomImage = images[Math.floor(Math.random() * images.length)];
-    } while (randomImage.includes(substring)); // Keep selecting if substring is found
-
-    return randomImage;
-  }
-
-  const closeMobileNav = () => {
-    const navbar = select('#navbar');
-    if (navbar.classList.contains('navbar-mobile')) {
-      navbar.classList.remove('navbar-mobile');
-      const navToggle = select('.mobile-nav-toggle');
-      navToggle.classList.toggle('bi-list');
-      navToggle.classList.toggle('bi-x');
-    }
-  };
-
+  
   /**
    * Reload home page
    */
   $(document).on('click', "#home-link", function () {
     // change focus
-    const focusLink = select('.active')
-    focusLink.classList.remove('active')
+    updateActiveTab(selectFn, '.home-link')
 
-    const newFocus = select('.home-link')
-    newFocus.classList.add('active')
-
-    const previousImageDiv = select('.previousImage')
+    const previousImageDiv = selectFn('.previousImage')
     const previousImage = previousImageDiv.classList[1]
     previousImageDiv.classList.remove(previousImage)
-
 
     //swap to home
     $("#content").fadeOut(200, function () {
@@ -63,31 +23,18 @@
         const newImage = randomImage.split("/")[2]
 
         // Get the home div and update its background image
-        const homeDiv = select('.bg-image')
+        const homeDiv = selectFn('.bg-image')
         homeDiv.style.backgroundImage = `url(${randomImage})`;
         previousImageDiv.classList.add(newImage)
         // fade in
         $("#content").fadeIn(200, function () {
           // startup typed now that page is loaded
-          const typed = select('.typed')
-          console.log(typed)
-          if (typed) {
-            let typed_strings = typed.getAttribute('data-typed-items')
-            console.log(typed_strings)
-            typed_strings = typed_strings.split(',')
-            new Typed('.typed', {
-              strings: typed_strings,
-              loop: true,
-              typeSpeed: 100,
-              backSpeed: 50,
-              backDelay: 2000
-            });
-          }
+          startTyped(selectFn)
         });
       });
     });
 
-    closeMobileNav()
+    closeMobileNav(selectFn)
     return false;
   });
 
@@ -96,18 +43,14 @@
    */
   $(document).on('click', '#about-link', function () {
     // change focus
-    const focusLink = select('.active')
-    focusLink.classList.remove('active')
-
-    const newFocus = select('.about-link')
-    newFocus.classList.add('active')
+    updateActiveTab(selectFn, '.about-link')
 
     $("#content").fadeOut(200, function () {
       $("#content").load("about.html", function () {
         $("#content").fadeIn(200);
       });
     });
-    closeMobileNav()
+    closeMobileNav(selectFn)
     return false;
   });
 
@@ -115,7 +58,7 @@
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = selectFn(el, all)
     if (selectEl) {
       if (all) {
         selectEl.forEach(e => e.addEventListener(type, listener))
@@ -130,7 +73,7 @@
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile')
+    selectFn('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
@@ -139,7 +82,7 @@
    * Mobile nav dropdowns activate
    */
   on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
+    if (selectFn('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
     }
@@ -148,19 +91,6 @@
   /**
    * Intro type effect
    */
-  const typed = select('.typed')
-  console.log(typed)
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    console.log(typed_strings)
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  startTyped(selectFn)
 
 })()
