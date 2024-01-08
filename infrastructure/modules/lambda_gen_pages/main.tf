d518d97faa6259da3a52a7ac6329a1290146777a
Jason England <jaengland1@gmail.com>
2024-01-07 23:40:03 -0600

    feat: add kms decrypt to lambda to upload s3

--- infrastructure/modules/lambda_gen_pages/main.tf
+++ infrastructure/modules/lambda_gen_pages/main.tf
@@ -52,7 +52,8 @@ data "aws_iam_policy_document" "lambda_policy" {
     sid = "AllowDecrypt"
     actions = [
       "kms:Decrypt",
-      "kms:Encrypt"
+      "kms:Encrypt",
+      "kms:GenerateDataKey"
     ]
     resources = [var.kms_arn]
     effect    = "Allow"
