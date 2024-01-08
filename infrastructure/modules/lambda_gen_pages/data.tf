ba79d576d4f8fac5c453e8760bf5e26a8e1b67a2
Jason England <jaengland1@gmail.com>
2024-01-07 20:40:32 -0600

    fix: archive_file output path

--- infrastructure/modules/lambda_gen_pages/data.tf
+++ infrastructure/modules/lambda_gen_pages/data.tf
@@ -1,6 +1,6 @@
 
 resource "archive_file" "lambda_gen_pages" {
-  output_path      = "/tmp/lambda_gen_pages.zip"
+  output_path      = "${path.root}/lambda_gen_pages.zip"
   output_file_mode = "0666"
   type             = "zip"
   source_dir       = "${path.module}/../../../src/lambda_gen_pages"
